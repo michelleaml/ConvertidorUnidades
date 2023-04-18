4a2255093f86f70570feb193a3e338f218c846e9
Ivan Sanchez <ivanss@cetys.edu.mx>
2023-04-18 00:28:18 -0700

    Agregue logica de conversion de unidades

--- app/src/main/java/com/example/convertidorunidades/MainActivity.java
+++ app/src/main/java/com/example/convertidorunidades/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.convertidorunidades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.convertidorunidades.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+//DatabaseOperations mDBOperations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //DatabaseOperations db = new DatabaseOperations(this);
+
+        //mDBOperations = new DatabaseOperations(MainActivity.this);
+
+        binding.btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, Converter.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
